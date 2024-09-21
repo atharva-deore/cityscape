65f684975d80c78dbaa90c2041b61e6f833948cf
atharva-deore <113436874+atharva-deore@users.noreply.github.com>
2024-09-21 23:40:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect("mongodb+srv://atharvadeore9934:HV7nFMQPfbuKKkJ6@cluster0.sce7gev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
@@ -13,6 +14,7 @@ mongoose.connect("mongodb+srv://atharvadeore9934:HV7nFMQPfbuKKkJ6@cluster0.sce7g
     console.log(err);
 });
 
+const __dirname=path.resolve();
 const app =express();
 
 app.use(express.json());
@@ -27,6 +29,10 @@ app.listen(3000 ,() =>{
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
 
 app.use((err,req,res,next) =>
 {
