cabf00a8e537f15acefd291349b33b42a6e543ff
atharva-deore <113436874+atharva-deore@users.noreply.github.com>
2024-09-21 23:23:50 +0530

    Add files via upload

---
+++ api/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+mongoose.connect("mongodb+srv://atharvadeore9934:HV7nFMQPfbuKKkJ6@cluster0.sce7gev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+    console.log('Connected to MongoDb!');
+}).catch((err) => {
+    console.log(err);
+});
+
+const app =express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000 ,() =>{
+    console.log('Server is running on port 3000 ');
+} 
+);
+
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+app.use((err,req,res,next) =>
+{
+    const statusCode=err.statusCode || 500;
+    const message =err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        sucess:false,
+        statusCode,
+        message,
+    });
+});
+
+app.get('/test', (req, res) => {
+    res.json({
+        message:'Hello World'});
+  });
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC25YouKvPEvdSqAC7Pbo-LWPlO6Ygpw5M",
+  authDomain: "estate-b7d60.firebaseapp.com",
+  projectId: "estate-b7d60",
+  storageBucket: "estate-b7d60.appspot.com",
+  messagingSenderId: "250639646935",
+  appId: "1:250639646935:web:c143e59ef5889459b43092"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
